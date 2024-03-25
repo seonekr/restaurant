b41c732528b280beeacd46849c37f2a24650d4a5
seonekr <zaemuevln42gmail.com>
2024-03-25 15:01:45 +0700

    push on branch

--- src/owner/page/Mainpage.jsx
+++ src/owner/page/Mainpage.jsx
@@ -16,8 +16,8 @@ import { FaPencil } from "react-icons/fa6";
 
 
 const Mainpage = () => {
-  
-    //  //PopUp box Delete
+
+    //  PopUp box Delete
     //  const [isPopupDelete, setPopupDelete] = useState(false);
 
     //  const handleDelete = () => {
